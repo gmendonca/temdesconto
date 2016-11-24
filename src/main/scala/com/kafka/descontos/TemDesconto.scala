18999432a8d501984db102264fc6c8e96e82a169
Gustavo Mendonca <gustavogmend@gmail.com>
2016-11-24 19:04:35 -0200

    Trying to using sink and commit messages

--- src/main/scala/com/kafka/descontos/TemDesconto.scala
+++ src/main/scala/com/kafka/descontos/TemDesconto.scala
@@ -8,6 +8,8 @@ import com.softwaremill.react.kafka.{ConsumerProperties, ReactiveKafka}
 import kafka.serializer.StringDecoder
 import org.reactivestreams.Publisher
 
+import scala.concurrent.Future
+
 /**
   * Created by gustavomendonca on 24/11/16.
   */
@@ -15,10 +17,11 @@ object TemDesconto extends {
 
   case class Config(zooKeeper: String = "localhost:2181",
                     topic: String = "",
-                    brokerList: String = "localhost:9092",
+                    brokerList: String = "localhost:6667",
                     groupId: String = "")
 
   def main(args: Array[String]) {
+
     val parser = new scopt.OptionParser[Config]("scopt") {
       head("Kafka tem desconto? Tem sim!", "1.0.0")
 
@@ -41,16 +44,24 @@ object TemDesconto extends {
       implicit val actorSystem = ActorSystem("ReactiveKafka")
       implicit val materializer = ActorMaterializer()
 
-      val kafka = new ReactiveKafka()
-      val publisher: Publisher[StringKafkaMessage] = kafka.consume(ConsumerProperties(
+      import actorSystem.dispatcher
+
+      val consumerProperties = ConsumerProperties(
         brokerList = config.brokerList,
         zooKeeperHost = config.zooKeeper,
         topic = config.topic,
         groupId = config.groupId,
         decoder = new StringDecoder()
-      ))
+      )
 
-      Source.fromPublisher(publisher).map(_.message()).map(m => println("Kafka tem desconto? Tem sim ó: " + m))
+      val kafka = new ReactiveKafka()
+
+      val consumer = kafka.consumeWithOffsetSink(consumerProperties)
+
+      Source.fromPublisher(consumer.publisher).mapAsync(1)(m =>{
+        println("Kafka tem desconto? Tem sim ó: " + m.message())
+        Future(m)
+      }).runWith(consumer.offsetCommitSink)
 
     } getOrElse {
       println("Do you know what you're doing? I don't think so!")
