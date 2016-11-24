2642b4659f913377cdc9596e76be4038ab3fa4e4
Gustavo Mendonca <gustavogmend@gmail.com>
2016-11-24 17:12:01 -0200

    Organizing parse of option and getting hot

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.11.0"
 
-libraryDependencies += "com.softwaremill.reactivekafka" %% "reactive-kafka-core" % "0.8.8"
+libraryDependencies ++= Seq(
+  "com.softwaremill.reactivekafka" %% "reactive-kafka-core" % "0.8.8",
+  "com.github.scopt"       %% "scopt" % "3.5.0"
+)
--- src/main/scala/com/gustavosystems/kafka/TemDesconto.scala
+++ src/main/scala/com/gustavosystems/kafka/TemDesconto.scala
@@ -12,24 +12,49 @@ import com.softwaremill.react.kafka.{ConsumerProperties, ProducerProperties, Rea
   * Created by gustavomendonca on 24/11/16.
   */
 object TemDesconto {
-  implicit val actorSystem = ActorSystem("ReactiveKafka")
-  implicit val materializer = ActorMaterializer()
 
-  val kafka = new ReactiveKafka()
-  val publisher: Publisher[StringKafkaMessage] = kafka.consume(ConsumerProperties(
-    brokerList = "localhost:9092",
-    zooKeeperHost = "localhost:2181",
-    topic = "lowercaseStrings",
-    groupId = "groupName",
-    decoder = new StringDecoder()
-  ))
-  val subscriber: Subscriber[String] = kafka.publish(ProducerProperties(
-    brokerList = "localhost:9092",
-    topic = "uppercaseStrings",
-    encoder = new StringEncoder()
-  ))
+  case class Config(zooKeeper: String = "localhost:2181",
+                    topic: String = "",
+                    brokerList: String = "localhost:9092",
+                    groupId: String = "")
 
-  Source.fromPublisher(publisher).map(_.message().toUpperCase)
-    .to(Sink.fromSubscriber(subscriber)).run()
+  def main(args: Array[String]) {
+    val parser = new scopt.OptionParser[Config]("scopt") {
+      head("Kafka Tem Desconto? Tem sim!", "1.0.0")
+
+      opt[String]('z', "zooKeeper") action { (x, c) =>
+        c.copy(zooKeeper = x) } text "zooKeeper host:port, otherwise localhost:2181 wil be used"
+
+      opt[String]('t', "topic") action { (x, c) =>
+        c.copy(topic = x) } text "Kafka topic name"
+
+      opt[String]('t', "brokerList") action { (x, c) =>
+        c.copy(brokerList = x) } text "Broker List"
+
+      opt[String]('t', "groupId") action { (x, c) =>
+        c.copy(groupId = x) } text "Kafka group Id"
+
+      help("help") text "prints this usage text"
+    }
+
+    parser.parse(args, Config()) map { config =>
+      implicit val actorSystem = ActorSystem("ReactiveKafka")
+      implicit val materializer = ActorMaterializer()
+
+      val kafka = new ReactiveKafka()
+      val publisher: Publisher[StringKafkaMessage] = kafka.consume(ConsumerProperties(
+        brokerList = config.brokerList,
+        zooKeeperHost = config.zooKeeper,
+        topic = config.topic,
+        groupId = config.groupId,
+        decoder = new StringDecoder()
+      ))
+
+      Source.fromPublisher(publisher).map(m => println("Kafka Tem Desconto? Tem sim ó: " + m.message()))
+
+    } getOrElse {
+      println("Do you know what you're doing? I don't think so!")
+    }
+  }
 
 }
